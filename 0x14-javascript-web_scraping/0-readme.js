1a4bd0dc52fdb007d27beb7b92e82369aec219ca
kimberlylikata <kimberlylikata@gmail.com>
2024-07-30 03:05:05 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,16 @@
+import sys
+
+def read_and_print_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <file_path>")
+    else:
+        file_path = sys.argv[1]
+        read_and_print_file(file_path)
